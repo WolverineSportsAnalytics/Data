6201a15c8a7199ed9f6e67f1827bf12571732414
Wolverine Sports Analytics <ciancioe@gmail.com>
2019-10-03 01:46:05 +0000

    See if cache works

--- ncaa/views.py
+++ ncaa/views.py
@@ -3,9 +3,21 @@ from django.http import HttpResponse
 from django.core.cache import cache
 from django.contrib.auth.decorators import login_required
 from get_tree import get_rankings
+from django.core.cache import cache
+from threading import Thread
+
 
 # Create your views here.
 def tree(request):
-    rankings = get_rankings()
+
+    rankings = cache.get('TreeRankings')
+    if not rankings:
+        thread = Thread(target=set_rankings_cache)
+        thread.start()
 
     return render(request, 'ncaa/tree.html', context={"rankings":rankings} )
+
+
+def set_rankings_cache():
+    rankings = get_rankings()
+    cache.set('TreeRankings', rankings, (6*60*60))
